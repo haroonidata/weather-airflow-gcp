2d3808e28e6a53a0895e0598d15acf30c69cb405
haroonidata <haroon.iqbal2442@gmail.com>
2026-06-08 22:26:33 +0100

    Added files

---
+++ dags/weather_dag.py
@@ -0,0 +1,20 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+import sys
+from weather_pipeline import run_pipeline
+
+sys.path.append("/opt/airflow/src")
+
+with DAG(
+    dag_id="weather_pipeline_dag",
+    start_date=datetime(2026, 6, 7),
+    schedule="@daily",
+    catchup=False,
+) as dag:
+
+    run_weather_pipeline = PythonOperator(
+        task_id="run_weather_pipeline",
+        python_callable=run_pipeline,
+    )
+

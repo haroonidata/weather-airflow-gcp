4a5cffc7cc6883ce1890d3e839f60fe6dfc147ae
haroonidata <haroon.iqbal2442@gmail.com>
2026-06-08 22:13:30 +0100

    Add Terraform for GCS and BigQusry resources

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 5.0"
-    }
-  }
-}
-
 provider "google" {
   project = "weather-ml-project-498711"
   region  = "europe-west2"
@@ -14,10 +5,10 @@ provider "google" {
 
 resource "google_storage_bucket" "weather_bucket" {
   name     = "haroon-weather-ml-498711"
-  location = "EU"
+  location = "EUROPE-WEST2"
 }
 
 resource "google_bigquery_dataset" "weather_dataset" {
   dataset_id = "weather_data"
-  location   = "EU"
+  location   = "europe-west2"
 }
